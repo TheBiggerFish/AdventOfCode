e572d91c51f83c7792a62910556f82f433c71cf7
TheBiggerFish <clhnumber4@gmail.com>
2021-04-23 23:03:46 -0400

    Problem 25 for 2015

---
+++ 2015/25/dec25.py
@@ -0,0 +1,22 @@
+# Written by Cameron Haddock
+# Written as a solution for Advent of Code 2015
+
+# https://adventofcode.com/2015/day/25
+
+
+from EulerLib.geometry import Point
+from EulerLib.numbers import triangle
+
+def get_diagonal_from_coords(pt:Point) -> int:
+    return pt.x + pt.y - 1
+
+def cycle(iter,initial):
+    for _ in range(1,iter):
+        initial *= 252533
+        initial %= 33554393
+    return initial
+
+
+pt = Point(3083,2978)
+rt = get_diagonal_from_coords(pt)
+print(cycle(triangle(rt-1)+pt.x,20151125))
