2721fc721aa6663b8ac84b7996502f73f7f19c65
TheBiggerFish <clhnumber4@gmail.com>
2021-04-23 23:02:58 -0400

    Problem 5 for 2015

--- 2015/05/dec05_2.py
+++ 2015/05/dec05_2.py
@@ -5,21 +5,15 @@
 
 
 import re
-from functools import reduce
-
-
-def and_reduce(a,b):
-    return a and b
 
 naughty,nice = 0,0
 with open('2015/05/input.txt') as f:
     for line in f:
-        condition1 = reduce(and_reduce,[])
-        condition2 = re.search()
+        condition1 = len(re.findall(r'(\w)(\w).*\1\2',line)) >= 1
+        condition2 = len(re.findall(r'(\w).\1',line)) >= 1
         if condition1 and condition2:
             nice += 1
         else:
             naughty += 1
-        
 
 print('There were {} nice strings and {} naughty strings'.format(nice,naughty))
