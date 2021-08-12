ec5f478538c178344b42288f49d85fbeb4b6ee26
TheBiggerFish <clhnumber4@gmail.com>
2021-08-11 23:11:43 -0400

    Completed 2016/19 part 2 very slowly

--- 2016/19/dec19.py
+++ 2016/19/dec19.py
@@ -4,6 +4,8 @@
 # https://adventofcode.com/2016/day/19
 
 
+from EulerLib.structures import Cycle
+
 input = 3001330
 
 def largest_power_of_two(num):
@@ -13,3 +15,19 @@ def largest_power_of_two(num):
     return pow_two >> 1
 
 print(f'The elf who receives all of the presents is elf {2*(input-largest_power_of_two(input))+1}')
+
+
+elves = Cycle(range(input))
+
+elf = 0
+while len(elves) > 1:
+    dead = (elf + len(elves)//2) % len(elves)
+    if len(elves) % 10**4 == 0:
+        print(f'{dead}/{len(elves)}')
+    del elves[dead]
+    if dead < elf:
+        elf -= 1
+    elf = (elf+1) % len(elves)
+    
+print(elves[0]+1)
+    
