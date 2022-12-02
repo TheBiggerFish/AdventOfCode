6108710b5fb296c9c3e213c732f3027568c06017
TheBiggerFish <clhnumber4@gmail.com>
2022-12-02 09:41:35 -0500

    Re-add part 1 output

--- 2022/01/dec01.py
+++ 2022/01/dec01.py
@@ -1,6 +1,7 @@
 from dataclasses import dataclass
 from functools import cached_property
 
+
 @dataclass
 class Elf:
     food: list[int]
@@ -12,8 +13,11 @@ class Elf:
     def __lt__(self, other: 'Elf') -> bool:
         return self.calories < other.calories
 
+
 with open('2022/01/input.txt') as f:
-    elves = [Elf(list(map(int,block.split()))) for block in f.read().split('\n\n')]
+    elves = [Elf(list(map(int, block.split())))
+             for block in f.read().split('\n\n')]
     top_3 = sorted(elves, reverse=True)[:3]
+    print(top_3[0].calories)
     total = sum(map(lambda elf: elf.calories, top_3))
     print(total)
