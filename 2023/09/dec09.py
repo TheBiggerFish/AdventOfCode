090f30226cb05c47da03c91a3c93544b46265b6e
TheBiggerFish <clhnumber4@gmail.com>
2023-12-09 00:46:35 -0500

    Remove weird object oriented structure

--- 2023/09/dec09.py
+++ 2023/09/dec09.py
@@ -1,40 +1,31 @@
-class OASIS:
-    def __init__(self, values = list[int]):
-        self.values = values
-            
-    @staticmethod
-    def diff_list(values: list[int]):
-        new = []
-        for i, value in enumerate(values):
-            if i+1 >= len(values):
-                break
-            new.append(values[i+1] - value)
-        return new
-    
-    @staticmethod
-    def extrapolate_next_value(values: list[int]) -> int:
-        if values.count(0) == len(values):
-            return 0
-        
-        diff_list = OASIS.diff_list(values)
-        return values[-1] + OASIS.extrapolate_next_value(diff_list)
-    
-    @staticmethod
-    def extrapolate_previous_value(values: list[int]) -> int:
-        if values.count(0) == len(values):
-            return 0
-        
-        diff_list = OASIS.diff_list(values)
-        return values[0] - OASIS.extrapolate_previous_value(diff_list)
-        
-        
+History = list[int]
+
+
+def diff_list(values: History):
+    new = []
+    for i, _ in enumerate(values[:-1]):
+        new.append(values[i+1] - values[i])
+    return new
+
+
+def extrapolate_next_value(values: History) -> int:
+    if values.count(0) == len(values):
+        return 0   
+    return values[-1] + extrapolate_next_value(diff_list(values))
+
+
+def extrapolate_previous_value(values: History) -> int:
+    if values.count(0) == len(values):
+        return 0
+    return values[0] - extrapolate_previous_value(diff_list(values))
+
 
 def main():
     with open('2023/09/input.txt') as f:
-        oases = [OASIS(list(map(int, line.split()))) for line in f.read().splitlines()]
+        histories = [list(map(int, line.split())) for line in f.read().splitlines()]
     
-    print(sum(OASIS.extrapolate_next_value(oasis.values) for oasis in oases))
-    print(sum(OASIS.extrapolate_previous_value(oasis.values) for oasis in oases))
+    print(sum(extrapolate_next_value(history) for history in histories))
+    print(sum(extrapolate_previous_value(history) for history in histories))
 
 
 if __name__ == '__main__':
