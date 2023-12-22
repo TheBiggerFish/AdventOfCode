69d2dbafd98eaf6ba6b8f5d198eda6e1356927d8
TheBiggerFish <clhnumber4@gmail.com>
2023-12-21 23:21:08 -0500

    Implemented solution for 2023.21

---
+++ 2023/21/dec21.py
@@ -0,0 +1,98 @@
+from fishpy.geometry import LatticePoint
+from fishpy.pathfinding.grid import Grid
+
+"""
+Types and counts of 'Cards', where each 'Card' is a single instance of the input grid
+
+       OSO
+      OE1EO
+     OE101EO
+    OE10101EO
+    SE01C101S
+    OE10101EO
+     OE101EO
+      OE1EO
+       OSO
+
+C: center (1) = some large odd number of cycles, stop counting after a while
+S: far side (4) = 65 steps in from middle of respective side
+O: outer ring odd = 65 steps in from respective corner
+E: outer ring even = 131+65 steps in from respective corner
+0: cell marked here as even, technically an odd number of steps overall, but only an even number within 'Card'
+1: cell marked here as odd, technically an even number of steps overall, but only an odd number within 'Card'
+
+O/E/0/1 cards are counted in terms of rings, where each ring has 4*d entries where d is the manhattan distance of each entry from the central card
+"""
+
+
+def exact_flood_fill(grid: Grid, start: LatticePoint, steps: int) -> set[LatticePoint]:
+    """
+        Return the set of points that can be reached from start after exactly the input number of steps
+        Each iteration, only step out from plots that were new in the last step (on the edge)
+    """
+    plots: dict[int, set[LatticePoint]] = {0: set(), 1: set()}
+    edge_plots: set[LatticePoint] = {start}
+    for i in range(1, steps+1):
+        new_plots: set[LatticePoint] = set()
+        for plot in edge_plots:
+            for adj in plot.get_adjacent_points(lower_bound=grid.bounds[0], upper_bound=grid.bounds[1]):
+                if grid[adj].rep == '.':
+                    new_plots.add(adj)
+        edge_plots = new_plots - plots[i%2]
+        plots[i%2] |= new_plots
+    return plots[steps%2]
+
+
+def main():
+    with open('2023/21/input.txt') as f:
+        lines = f.read().splitlines()
+    
+    grid = Grid.from_list_of_strings(lines)
+    start = grid.char_positions('S')['S'].pop()
+    grid[start].rep = '.'
+    
+    plots = exact_flood_fill(grid, start, steps=130)
+    print(len(plots))
+    
+    #65+even=7520, 65+odd=7457
+    filled_even = len(exact_flood_fill(grid, start, steps=(65+131+131))) #7520
+    filled_odd = len(exact_flood_fill(grid, start, steps=(65+131+131+1))) #7457
+    
+    far_left = len(exact_flood_fill(grid, LatticePoint(131, 65), steps=131))
+    far_right = len(exact_flood_fill(grid, LatticePoint(-1, 65), steps=131))
+    far_up = len(exact_flood_fill(grid, LatticePoint(65, 131), steps=131))
+    far_down = len(exact_flood_fill(grid, LatticePoint(65, -1), steps=131))
+
+    far_bottom_right_odd = len(exact_flood_fill(grid, LatticePoint(-1,0), steps=65))
+    far_top_right_odd = len(exact_flood_fill(grid, LatticePoint(-1,130), steps=65))
+    far_bottom_left_odd = len(exact_flood_fill(grid, LatticePoint(131,0), steps=65))
+    far_top_left_odd = len(exact_flood_fill(grid, LatticePoint(131,130), steps=65))
+
+    far_bottom_right_even = len(exact_flood_fill(grid, LatticePoint(-1,0), steps=(131 + 65)))
+    far_top_right_even = len(exact_flood_fill(grid, LatticePoint(-1,130), steps=(131 + 65)))
+    far_bottom_left_even = len(exact_flood_fill(grid, LatticePoint(131,0), steps=(131 + 65)))
+    far_top_left_even = len(exact_flood_fill(grid, LatticePoint(131,130), steps=(131 + 65)))
+    
+    far_steps = 26501365
+    options = filled_even # center
+    options += far_left + far_right + far_up + far_down
+    
+    ortho = (far_steps - 65) // 131  #  =202300
+    for ring in range(1,ortho):
+        if ring%2 == 0:
+            options += 4 * ring * filled_even
+        else:
+            options += 4 * ring * filled_odd
+
+    # Outer ring even
+    options += (ortho-1) * (far_bottom_right_even + far_top_right_even + far_bottom_left_even + far_top_left_even)
+    # Outer ring odd
+    options += ortho * (far_bottom_right_odd + far_top_right_odd + far_bottom_left_odd + far_top_left_odd)
+    
+    print(options)
+    
+    
+
+
+if __name__ == '__main__':
+    main()
